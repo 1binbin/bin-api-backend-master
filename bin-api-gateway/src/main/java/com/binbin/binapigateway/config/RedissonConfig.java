445054e9a0b838ea4fbd94f1111b7d3514051060
hongxiaobin <2812181610@qq.com>
2023-07-13 18:50:06 +0800

    fix: 修复redis缺失密码以及gateway启动没有排除数据库

--- bin-api-gateway/src/main/java/com/binbin/binapigateway/config/RedissonConfig.java
+++ bin-api-gateway/src/main/java/com/binbin/binapigateway/config/RedissonConfig.java
@@ -26,13 +26,15 @@ import org.springframework.context.annotation.Configuration;
 public class RedissonConfig {
     private String host;
     private String port;
+    private String password;
+    private Integer database;
 
     @Bean
-    public RedissonClient redissonClient(){
+    public RedissonClient redissonClient() {
         // 1. 创建配置
         Config config = new Config();
         String redisAddress = String.format("redis://%s:%s", host, port);
-        config.useSingleServer().setAddress(redisAddress).setDatabase(3);
+        config.useSingleServer().setAddress(redisAddress).setDatabase(database).setPassword(password);
         // 2. 创建实例
         return Redisson.create(config);
     }
