1c1be08a3e7c67885bef57710968a6b6520ec7da
hongxiaobin <2812181610@qq.com>
2023-07-13 10:04:54 +0800

    feat: 添加redis相关配置器

---
+++ bin-api-gateway/src/main/java/com/binbin/binapigateway/config/RedisTemplateConfig.java
@@ -0,0 +1,60 @@
+/*
+ * com.binbin.binapigateway.config.RedisTemplateConfig, 2023-07-13
+ * Copyright© 2023 hongxiaobin(1binbin),Inc. All rights reserved.
+ * Github link : http://github.com/1binbin
+ */
+
+package com.binbin.binapigateway.config;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * RedisTemplate配置器
+ * @Author hongxiaobin
+ * @Time 2023/7/13 0013-10:01:23
+ */
+@Configuration
+public class RedisTemplateConfig {
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(factory);
+        Jackson2JsonRedisSerializer<Object> jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<>(Object.class);
+        ObjectMapper mapper = new ObjectMapper();
+
+        //解决jackson反序列化localdatetime
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.registerModule(new JavaTimeModule());
+        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+
+        // mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        mapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance,
+                ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+        jackson2JsonRedisSerializer.setObjectMapper(mapper);
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+
+        // key采用String的序列化方式
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+        // hash的key也采用String的序列化方式
+        redisTemplate.setHashKeySerializer(stringRedisSerializer);
+        // value序列化方式采用jackson
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        // hash的value序列化方式采用jackson
+        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.afterPropertiesSet();
+
+        return redisTemplate;
+    }
+}
---
+++ bin-api-gateway/src/main/java/com/binbin/binapigateway/config/RedissonConfig.java
@@ -0,0 +1,39 @@
+/*
+ * com.binbin.binapigateway.config.RedissonConfig, 2023-07-13
+ * Copyright© 2023 hongxiaobin(1binbin),Inc. All rights reserved.
+ * Github link : http://github.com/1binbin
+ */
+
+package com.binbin.binapigateway.config;
+
+import lombok.Data;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Redisson配置器
+ *
+ * @Author hongxiaobin
+ * @Time 2023/7/13 0013-10:01:11
+ */
+@Configuration
+@ConfigurationProperties(prefix = "spring.redis")
+@Data
+public class RedissonConfig {
+    private String host;
+    private String port;
+
+    @Bean
+    public RedissonClient redissonClient(){
+        // 1. 创建配置
+        Config config = new Config();
+        String redisAddress = String.format("redis://%s:%s", host, port);
+        config.useSingleServer().setAddress(redisAddress).setDatabase(3);
+        // 2. 创建实例
+        return Redisson.create(config);
+    }
+}
